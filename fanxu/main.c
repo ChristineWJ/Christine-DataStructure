b87fbc1644a42053439c1f915fc477d00a63871a
ChristineWJ <35292152+ChristineWJ@users.noreply.github.com>
2018-03-21 15:41:15 +0800

    update

---
+++ fanxu/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int revstr(char str[])
+{
+    int n = strlen(str);
+    int i,j;
+    char temp;
+    for(i=0;i<n-i-1;i++){
+        temp = str[i];
+        str[i] = str[n-1-i];
+        str[n-1-i] = temp;
+    }
+
+    return 0;
+}
+int main()
+{
+    char str[256] = "hello world!";
+    revstr(str);
+    puts(str);
+    return 0;
+}
